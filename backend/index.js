b0714a9570b8f790fe4cacff1e8f1a9bd44be05d
Anish <xevenbiswas@gmail.com>
2023-11-17 22:58:10 +0530

    Update PORT variable to use environment variable

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ app.use(express.json());
 // Define Routes
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
-
-// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
