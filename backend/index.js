84b597c630966183acd06f81b688a499a7a0ec6a
Anish <xevenbiswas@gmail.com>
2023-11-17 21:20:32 +0530

    Add CORS configuration for example.com

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const app = express();
 
 // Connect to database
 connectDB();
-app.use(cors());
+app.use(cors({
+    origin: 'https://example.com',
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 
 app.use(express.json());
 
