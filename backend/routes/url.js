fb08da09403f1a8b3c4755be85fa9de81e82de8b
Anish <xevenbiswas@gmail.com>
2023-11-18 13:38:33 +0530

    nano to 2 , just for test

--- backend/routes/url.js
+++ backend/routes/url.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // @route  POST /api/url/shorten
 // @desc  Create short URL
-const num = 9;
+const num = 2;
 
 //baseUrl for backend url
 const baseUrl = process.env.NODE_ENV === 'production' 
