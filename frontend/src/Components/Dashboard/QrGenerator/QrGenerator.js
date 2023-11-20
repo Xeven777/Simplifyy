bf6b93b546715334d17541d94913cd4dbbea8722
Anish <xevenbiswas@gmail.com>
2023-11-20 10:30:42 +0530

    uff working

--- frontend/src/Components/Dashboard/QrGenerator/QrGenerator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QrGenerator = () => {
-  return (
-    <div className='min-h-screen bg-black'>QrGenerator</div>
-  )
-}
-
-export default QrGenerator
---
+++ frontend/src/Components/Dashboard/QrGenerator/QrGenerator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QrGenerator = () => {
+  return <div className="min-h-screen bg-black">QrGenerator</div>;
+};
+
+export default QrGenerator;
