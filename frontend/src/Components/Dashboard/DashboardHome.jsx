637340df4138eff1538e4462e0144334c4fcb66a
Anish <xevenbiswas@gmail.com>
2023-11-27 22:15:58 +0530

    m

--- frontend/src/Components/Dashboard/DashboardHome.jsx
+++ frontend/src/Components/Dashboard/DashboardHome.jsx
@@ -21,12 +21,10 @@ const DashboardHome = () => {
       <div className="lg:w-[70%] max-w-[90%]">
         <div className="greeting-box text-left md:w-full mt-36 md:pr-[100px] h-fit md:px-8 prounded-xl relative overflow-hidden rounded-xl py-6 px-2 leading-relaxed">
           <h1 className="text-3xl font-medium text-zinc-100 ">{greeting}</h1>
-          <h2 className="text-2xl mt-6">
-            What do you want to do today?
-          </h2>
+          <h2 className="text-2xl mt-6">What do you want to do today?</h2>
         </div>
 
-        <div className="grid gap-4 py-8 grid-cols-2">
+        <div className="grid gap-4 py-8 md:grid-cols-2 grid-cols-1">
           <Link
             to="/dashboard/url-shortener"
             className="bg-gray-900 h-48 bg-opacity-30 border-s border-e border-slate-950 transition hover:border-slate-700 text-white font-bold px-6 rounded-2xl text-3xl py-10 simple-box"
