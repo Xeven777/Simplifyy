7f4b43830cf24b765f361ab196defd9349940ed6
Anish <xevenbiswas@gmail.com>
2024-01-16 18:15:10 +0530

    app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [react(), VitePWA()]
+})
